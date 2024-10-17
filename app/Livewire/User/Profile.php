141d6440aee0bb374ac322d16e24f93a56a3f173
Zaw Thet Paing <zawthetpaing333@gmail.com>
2024-10-17 21:11:40 +0630

    user change password

--- app/Livewire/User/Profile.php
+++ app/Livewire/User/Profile.php
@@ -4,16 +4,23 @@ namespace App\Livewire\User;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Profile extends Component
 {
 
+    // edit profile
     public $editName;
     public $editEmail;
 
     public $currentEmail;
     public $currentName;
+
+    // change password
+    public $oldPassword;
+    public $newPassword;
+    public $confirmPassword;
 
     public function mount()
     {
@@ -33,6 +40,7 @@ class Profile extends Component
     public function resetEditProfileErrors()
     {
         $this->resetErrorBag('editEmail');
+        $this->resetValidation();
     }
 
     public function updateProfile()
@@ -65,6 +73,38 @@ class Profile extends Component
         }
         // dd($this->name, $this->email);
         session()->flash('alert', 'User information updated!');
+    }
+
+    //change password
+
+    public function resetPasswordForm()
+    {
+        $this->oldPassword = '';
+        $this->newPassword = '';
+        $this->confirmPassword = '';
+    }
+
+    public function changePassword()
+    {
+        $this->validate([
+            'oldPassword'=> 'required',
+            'newPassword'=> 'required|min:8',
+            'confirmPassword'=> 'required|same:newPassword'
+        ]);
+
+        $auth_user = Auth::user();
+
+        if(!Hash::check($this->oldPassword, $auth_user->password)){
+            $this->addError('oldPassword', 'Old Password didn\'t match');
+            return;
+        }
+
+        $user = User::find($auth_user->id);
+        $user->update([
+            'password'=> Hash::make($this->confirmPassword)
+        ]);
+
+        session()->flash('changePasswordAlert', 'Password Changed!');
 
     }
 
--- resources/views/livewire/user/profile.blade.php
+++ resources/views/livewire/user/profile.blade.php
@@ -13,7 +13,7 @@
                                 <button :class="navStatus === 'editProfile' ? 'nav-link active' : 'nav-link'" x-on:click="navStatus = 'editProfile'; $wire.resetEditProfileErrors()">Edit Profile</button>
                             </li>
                             <li class="nav-item">
-                                <button :class="navStatus === 'changePassword' ? 'nav-link active' : 'nav-link'" x-on:click="navStatus = 'changePassword'">Change Password</button>
+                                <button :class="navStatus === 'changePassword' ? 'nav-link active' : 'nav-link'" x-on:click="navStatus = 'changePassword'; $wire.resetPasswordForm()">Change Password</button>
                             </li>
                         </ul>
                     </div>
@@ -62,19 +62,39 @@
                         </div>
 
                         <div :class="navStatus === 'changePassword' ? 'd-block' : 'd-none'">
+                             <!-- Display the flash message if it exists -->
+                             @if (session()->has('changePasswordAlert'))
+                             <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                 {{ session('changePasswordAlert') }}
+                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                             </div>
+                         @endif
                             <h2 class="text-center">Change Password</h2>
-                            <form>
+                            <form x-data="{ status: false }" wire:submit.prevent="changePassword">
                                 <div class="mb-3">
                                     <label for="">Old Password : </label>
-                                    <input type="password" class="form-control">
+                                    <div class="input-group">
+                                        <input :type="status ? 'text' : 'password'" wire:model="oldPassword" class="form-control">
+                                        <span class="input-group-text" style="cursor: pointer" x-on:click="status = !status" x-text="status ? 'Hide' : 'Show'">
+                                        </span>
+                                    </div>
+                                    @error('oldPassword')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <label for="">New Password : </label>
-                                    <input type="password" class="form-control">
+                                    <input :type="status ? 'text' : 'password'" wire:model="newPassword" class="form-control">
+                                    @error('newPassword')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <label for="">Confirm Password : </label>
-                                    <input type="password" class="form-control">
+                                    <input :type="status ? 'text' : 'password'" wire:model="confirmPassword" class="form-control">
+                                    @error('confirmPassword')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-secondary w-100">Change Password</button>
